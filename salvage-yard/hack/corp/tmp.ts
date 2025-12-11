717dad338a34d9274050b5d3d1e9e18b6fb15f85
Andrey Andreyevich Bienkowski <hexagonrecursion@gmail.com>
2025-12-11 11:08:56 +0300

    Delete salvage-yard/hack/corp directory

--- salvage-yard/hack/corp/tmp.ts
+++
@@ -1,7 +0,0 @@
-export async function main(ns: NS) {
-  const {producedMaterials} = ns.corporation.getIndustryData('Agriculture');
-  for(const m of producedMaterials) {
-    ns.tprint(ns.corporation.getMaterialData(m));
-  }
-  
-}
